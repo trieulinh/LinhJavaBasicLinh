67281c754ce445b8561e689abaff23d8781d27dd
trieulinh <nguyenthuytrieulinh@gmail.com>
2023-06-13 07:16:05 +0700

    Comment unnessesary thing

--- src/Variable/Variables01.java
+++ src/Variable/Variables01.java
@@ -11,9 +11,9 @@ package Variable;
             String ID = "1234";
             System.out.println("ID: " + ID + " " + "Tên : "+name01 + " " + "Tuổi: " +age + " " + school);
         };
-        static {
-            System.out.println("1234");;
-        }
+        //static {
+          //  System.out.println("1234");;
+        //}
 
         public static void main(String[] args) {
             Variables01 sv01 = new Variables01("Phạm Thị Thu Phương", 30);
